ab4fb3246d75b8f28515649ec6eacc671c027f4a
David <david.l.larsson.90@gmail.com>
2020-06-18 13:44:57 +0200

    read user data properly

--- src/Pages/SettingsPage.js
+++ src/Pages/SettingsPage.js
@@ -5,66 +5,85 @@ import '../Styles/SettingsPage.css'
 
 const SettingsPage = () => {
 
-
-    const [userData, setUserData] = useState();
-    const [firstName, setSetFirstName] = useState();
-
+    const [userDataArr, setUserDataArr] = useState([JSON.parse(localStorage.getItem('current_user'))]);
+    const [firstName, setFirstName] = useState();
+    const [lastName, setLastName] = useState();
+    const [addressState, setAddress] = useState([""]);   
+    const [personNr, setPersonNr] = useState("");
+    const [zipCodeState, setZipCode] = useState("");
+    const [cityState, setCity] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [email, setEmail] = useState("");
+    
     useEffect(() => {
-        fetch("https://localhost:5001/user/5")
-      .then(res => res.json())
-      .then(result => {
-            setUserData(result);
-            setSetFirstName(result.firstName)
-        },
-        (error) => {
-          console.log(error)
-          });
-        }
-    , []);
-
-    const handleChange = (e) => {
+            const user = userDataArr[0];
+            const {addressRow1, zipCode, city} = user.address[0];
+            const full = user.fullName.split(' ');
+            setZipCode(zipCode);
+            setAddress(addressRow1);
+            setCity(city);
+            setEmail(user.email);
+            setPhoneNumber("0000000000");
+            setPersonNr(user.personNr);
+            setFirstName(full[0]);
+            setLastName(full[1]);
+            console.log(user.address);
+        },[userDataArr]);
         
-        console.log(e.target.name)
-        
+        const handleChange = (e) => {
+            const u = userDataArr[0];
+            if (e.target.name === "addressRow1" || e.target.name === "city" || e.target.name === "zipCode"){
+                u.address[0][e.target.name] = e.target.value
+            } else if (e.target.name === "firstName") {
+                u["fullName"] = `${e.target.value} ${lastName}`;
+            } else if (e.target.name === "lastName"){
+                u["fullName"] = `${firstName} ${e.target.value}`;
+            }
+            else {
+                u[e.target.name] = e.target.value;
+            }
+            setUserDataArr([u]);
+            console.log(u);
     };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
     return (
-        
     <div className="settingsPageAll">
-
     <h3>Settings</h3>
        <div className="settingsContent">
        <SettingsNav />
            <img src={defaultProfile} style={{height: 90, width: 90}} alt="profilePicture"></img>
-           <form>
+           <form onSubmit={handleSubmit}>
                <div className="settingsRow">
                    <div>
                        <label>First Name</label><br></br>
-                       <input name="firstName" onChange={handleChange} value={firstName}></input>
+                       <input onChange={handleChange} name="firstName" value={firstName}></input>
                    </div>
                    <div>
                        <label>Last name</label><br></br>
-                       <input onChange={handleChange} value={userData && userData.lastName ? userData.lastName : "No info" }></input>
+                       <input onChange={handleChange} name="lastName" value={lastName}></input>
                    </div>
                </div>
-               <label>Social security number</label><br></br>
-               <input onChange={handleChange} value={userData && userData.personNr ? userData.personNr : "No info" }></input><br></br>
+               <label>Person number</label><br></br>
+               <input onChange={handleChange} name="personNr" value={personNr}></input><br></br>
                <label>Address</label><br></br>
-               <input onChange={handleChange}></input><br></br>
+               <input onChange={handleChange} value={addressState} name="addressRow1"></input><br></br>
                <div className="settingsRow">
                    <div>
-                       <label>Zip code</label><br></br>
-                       <input onChange={handleChange}></input>
+                       <label>Zip code</label><br></br> 
+                       <input onChange={handleChange} name="zipCode" value={zipCodeState}></input>
                    </div>
                    <div>
                        <label>City</label><br></br>
-                       <input onChange={handleChange}></input>
+                       <input onChange={handleChange} name="city" value={cityState}></input>
                    </div>
                </div>
                <label>Phone number</label><br></br>
-               <input onChange={handleChange}></input><br></br>
+               <input onChange={handleChange} name="phoneNumber" value={phoneNumber}></input><br></br>
                <label>Email</label><br></br>
-               <input onChange={handleChange} value={userData && userData.email ? userData.email : "No info" }></input>
+               <input onChange={handleChange} value={email}></input>
                <input type="submit"></input>
            </form>
        </div>
