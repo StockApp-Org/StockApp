488390cf7c458deedcedb883675f4c73048a364d
Daniel Åslund <daniel.aaslund@gmail.com>
2020-06-20 00:17:06 +0200

    Added suppression of warnings for homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 import React, {useState, useEffect} from 'react';
 import {NavLink} from 'react-router-dom'
 import {Container, Row, Col, Toast, Button} from 'react-bootstrap';
