02acc15b8777ce2121bdebca3bf5de38ab508938
Daniel Åslund <daniel.aaslund@gmail.com>
2020-06-24 10:04:43 +0200

    Added styling to portfolio page and moved footer to be at bottom of page

--- src/Pages/PortfolioPage.js
+++ src/Pages/PortfolioPage.js
@@ -1,11 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Container, Table} from 'react-bootstrap'
 import StockTableBody from '../Components/StockTableBody'
+import '../Styles/PortfolioPage.css';
 
 let PortfolioPage = () => {
 
     return(
-        <Container>
+        <Container id="portfoliotable">
             <Table bordered striped hover variant="dark">
                 <thead>
                     <tr>
