65d7e263e99ce57d79285ccbc5a4e0bb9b539a32
David <david.l.larsson.90@gmail.com>
2020-06-26 19:42:22 +0200

    added delete user function

--- src/Pages/GdprPage.js
+++ src/Pages/GdprPage.js
@@ -1,29 +1,83 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../Components/SettingsNav'
 import Config from '../Config/config.json'
 import '../Styles/GdprPage.css';
+import { Redirect } from 'react-router-dom';
+
 
 let GdprPage = () => {
 
     const ApiUrlWithPort = Config.ApiUrl+':'+Config.ApiPort;
-    const userId = JSON.parse(localStorage.getItem('current_user')).userId;
+    const user =  JSON.parse(localStorage.getItem('current_user'));
+
+    const userId = user.userId;
+    
+    const [password, setPassword] = useState();
+    const [confirmPassword, setConfirmPassword] = useState();
+    const [loggedOut, setLoggedOut] = useState(false);
+
 
     const [deleteUserActive, setDeleteUserActive] = useState();
-    const handleClick = () => setDeleteUserActive((
-        <>
-            <form className="deleteForm" onSubmit={handleSubmit}>
-                <label>Password: </label><input></input>
-                <label>Confirm password: </label><input></input>
-                <input type="submit"></input>
-            </form>
-        </>
-    ));
+
+    
+    const handleChange = e => {
+
+            if(e.target.name === "pw"){
+                setPassword(e.target.value);
+            } else if (e.target.name === "cpw"){
+                setConfirmPassword(e.target.value);
+            }
+        }
+        console.log(password);
+        console.log(confirmPassword);
+   
+
+    const handleClick = () => setDeleteUserActive(true);
+
+
+    
+    const deleteUser = () => {
+        var formData = new FormData();
+        formData.append("userId", userId)
+        formData.append("password", password);
+        let userAddress = "";
+        if (user.address[0]){
+            userAddress = user.address[0].userId;
+        }
+        console.log(userAddress);
+        formData.append("passwordSalt", userAddress);
+        console.log(user.address[0])
+
+        var req = {
+            method: 'DELETE',
+            body: formData
+        };
+        console.log(user.address);
+        
+            fetch(ApiUrlWithPort+"/user/delete", req)
+            .then(response => response.json())
+            .then(data => {
+                setLoggedOut(true);
+            })
+            .catch(err => {
+                alert("Wrong password!");
+            });
+    }
+
+
 
     const handleSubmit = e => {
         e.preventDefault();
+
+        if (password === confirmPassword){
+            deleteUser();
+        } else {
+            alert("Passwords don't match");
+        }
     }
 
-    return (
+    
+    return loggedOut ? <Redirect push to="/" /> : (
         <div className="preferencesPage">
         <h3>GDPR</h3>
         <div className="gdprContent">
@@ -38,7 +92,15 @@ let GdprPage = () => {
                     </li>
                 </ul>
             </div>
-                        {deleteUserActive && deleteUserActive}
+                        {
+                        deleteUserActive && 
+                        <>
+                        <form className="deleteForm" onSubmit={handleSubmit}>
+                            <label>Password: </label><input name="pw" onChange={handleChange}></input>
+                            <label>Confirm password: </label><input name="cpw" onChange={handleChange}></input>
+                            <input type="submit"></input>
+                        </form>
+                         </>}
         </div>
     </div>
     )
