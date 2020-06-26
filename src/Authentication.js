0a420e8a7ea7129646a37abdbbb24851c003d103
Daniel Åslund <daniel.aaslund@gmail.com>
2020-06-26 17:35:14 +0200

    resolved bug with empty repsonse body on invalid sign in

--- src/Authentication.js
+++ src/Authentication.js
@@ -21,17 +21,20 @@ export default class Authentication {
             .then(data => {
                 if(data !== "" && data != null) {
                 let expiresAt = JSON.stringify(new Date().addHours(6));
+                var jsonData = JSON.parse(data);
+                console.log(jsonData);
                 localStorage.setItem('current_user', JSON.stringify({
-                    userId: data.userId,
-                    fullName: data.firstName + ' ' + data.lastName,
-                    email: data.email,
-                    personNr: data.personNr,
-                    orgNr: data.orgNr,
-                    imgUrl: data.imgUrl,
-                    address: data.userAddress,
-                    phone: data.phoneNumber,
-                    shares: data.userShares
-                    }));
+                    userId: jsonData.userId,
+                    fullName: jsonData.firstName + ' ' + jsonData.lastName,
+                    email: jsonData.email,
+                    personNr: jsonData.personNr,
+                    orgNr: jsonData.orgNr,
+                    imgUrl: jsonData.imgUrl,
+                    address: jsonData.userAddress,
+                    phone: jsonData.phoneNumber,
+                    shares: jsonData.userShares
+                    })
+                    );
                     localStorage.setItem('expires_at', expiresAt);
                 }
                 resolve(data);
