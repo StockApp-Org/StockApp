5cf6cf85e6c26f134a53cb9eab30e5a2bc7cd0ae
David <david.l.larsson.90@gmail.com>
2020-06-26 15:12:03 +0200

    added delete button

--- src/Pages/GdprPage.js
+++ src/Pages/GdprPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../Components/SettingsNav'
 import Config from '../Config/config.json'
 import '../Styles/GdprPage.css';
@@ -8,14 +8,37 @@ let GdprPage = () => {
     const ApiUrlWithPort = Config.ApiUrl+':'+Config.ApiPort;
     const userId = JSON.parse(localStorage.getItem('current_user')).userId;
 
+    const [deleteUserActive, setDeleteUserActive] = useState();
+    const handleClick = () => setDeleteUserActive((
+        <>
+            <form className="deleteForm" onSubmit={handleSubmit}>
+                <label>Password: </label><input></input>
+                <label>Confirm password: </label><input></input>
+                <input type="submit"></input>
+            </form>
+        </>
+    ));
+
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
+
     return (
         <div className="preferencesPage">
-        <h3>Preferences</h3>
+        <h3>GDPR</h3>
         <div className="gdprContent">
             <Navbar /> 
             <div>
-                <a href={ApiUrlWithPort+'/User/Download/'+userId}>Download Your Data</a>
+                <ul>
+                    <li>
+                        <a href={ApiUrlWithPort+'/User/Download/'+userId}>Download Your Data</a>
+                    </li>
+                    <li>
+                        <button onClick={handleClick}>Delete your user</button>
+                    </li>
+                </ul>
             </div>
+                        {deleteUserActive && deleteUserActive}
         </div>
     </div>
     )
