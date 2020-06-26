66eceb2696cdbe3639b661528b688f3c270058b6
Daniel Åslund <daniel.aaslund@gmail.com>
2020-06-26 14:14:38 +0200

    Added animation to login page and home page

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -9,36 +9,33 @@ let LoginPage = (props) => {
     
     return (
         <Container className="loginPageContainer">
-                
+            <Row>
+                <Col lg={5} id="loginDiv" className="form-div top-padding-5">
                     <Row>
-                        <Col lg={5} id="loginDiv" className="form-div top-padding-5">
-                            <Row>
-                                <Col lg={{offset: 2, span: 6}}>
-                                    <h1>User Login</h1>
-                                </Col>
-                            </Row>
-                            <Row>
-                                <LoginForm auth={props.auth}/>
-                            </Row>
-                            <Row>
-                                <a href="/">Forgot your password? Click here!</a>
-                            </Row>
-                        </Col>
-                        <Col lg={2}>
-                            <img src={Logo} alt="Logo"/>
-                        </Col>
-                        <Col lg={5} id="signUpDiv" className="form-div top-padding-5">
-                            <Row>
-                                <Col lg={{offset: 2, span: 5}}>
-                                    <h1>Sign Up</h1>
-                                </Col>
-                            </Row>
-                            <Row>
-                                <SignUpForm/>
-                            </Row>
+                        <Col lg={{offset: 2, span: 6}}>
+                            <h1>User Login</h1>
                         </Col>
                     </Row>
-                
+                    <Row>
+                        <LoginForm auth={props.auth}/>
+                    </Row>
+                    <Row>
+                        <a href="/">Forgot your password? Click here!</a>
+                    </Row>
+                </Col>
+                <Col lg={2}>
+                </Col>
+                <Col lg={5} id="signUpDiv" className="form-div top-padding-5">
+                    <Row>
+                        <Col lg={{offset: 2, span: 5}}>
+                            <h1>Sign Up</h1>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <SignUpForm/>
+                    </Row>
+                </Col>
+            </Row>
         </Container>
         );
 }    
